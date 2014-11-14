813d45a4fa0d63d548b71f2e38a1418dee3d67ce
Paul Schkade <dev@paulschkade.net>
2014-11-14 23:41:43 +0100

    aufräumen

--- js/main.js
+++ js/main.js
@@ -4,8 +4,16 @@ function DotHivController() {
 
 	var self = this;
 
+	// see http://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser
+	var isIE = /*@cc_on!@*/false || !!document.documentMode; // At least IE6
+
 	self.initUIBehaviour = function() {
 		$('.dot').removeClass('pre-init');
+
+		// if(isIE){
+		// 	$('.dot').removeClass('flip-container');
+		// 	console.log('switch');
+		// }
 	};
 
 	self.initUIDefaultSettings = function() {
