6b282f878cbc8ba9c1b0b3faed3c7dfb788c1ab8
Paul Schkade <dev@paulschkade.net>
2014-11-15 17:21:32 +0100

    sharing + behavior tracking

--- js/main.js
+++ js/main.js
@@ -8,19 +8,42 @@ function DotHivController() {
 	var isIE = /*@cc_on!@*/false || !!document.documentMode; // At least IE6
 
 	self.initUIBehaviour = function() {
+		// eindrehen bei Start
 		$('.dot').removeClass('pre-init');
 
+		// IE bugfixing
 		if(isIE){
 			$('.dot').addClass('ie');
 		}
+
+		// behaviour tracking
+		
+		$('.share-button.fb').on('click', function() {
+			ga('send', 'event', 'button', 'click', 'click on facebook share');
+		});
+
+		$('.share-button.twitter').on('click', function() {
+			ga('send', 'event', 'button', 'click', 'click on twitter share');
+
+		});
+
+		// wird bei Klick auf FB und Twitter auch ausgelöst und macht wegen Hover-Interaktion nicht viel Sinn
+		// $('.dot1').on('click touchstart', function() {
+		// 	ga('send', 'event', 'button', 'click', 'click on circle 1');
+		// });
+
+		$('.dot2link').on('click', function() {
+			ga('send', 'event', 'button', 'click', 'click on deeplink in circle 2');
+		});
+
+		$('.dot3link').on('click', function() {
+			ga('send', 'event', 'button', 'click', 'click on deeplink in circle 3');
+		});
+
 	};
 
-	self.initUIDefaultSettings = function() {
-
-	};
 
 	self.initUIBehaviour();
-	self.initUIDefaultSettings();
 }
 
 $(document).ready(function() {
